92e93770dcb2d4c3e7eab2b23c5b4fc706829675
Zeljko Vrbaski <zeljko.vrbaski@sbgenomics.com>
2017-04-05 10:10:02 +0200

    added example for MagicMock test

---
+++ seventweets/tests/test_mock.py
@@ -0,0 +1,20 @@
+from unittest import mock
+
+import requests
+
+
+def return_my_ip():
+    response = requests.get('https://httpbin.org/ip')
+    return response.json()['origin']
+
+
+def test_return_my_ip():
+    mock_get_request = mock.MagicMock()
+    with mock.patch('requests.get', new=mock_get_request):
+        ip = return_my_ip()
+
+    mock_get_request.assert_called_once_with('https://httpbin.org/ip')
+    mock_response = mock_get_request.return_value
+    obj = mock_response.json.return_value
+    obj.__getitem__.assert_called_once_with('origin')
+    assert obj.__getitem__.return_value == ip
