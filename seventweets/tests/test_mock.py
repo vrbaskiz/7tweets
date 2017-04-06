c27b9a05c53b36c4cc336226da8159bb7bb07d71
Zeljko Vrbaski <zeljko.vrbaski@sbgenomics.com>
2017-04-06 10:09:17 +0200

    new assertion added

--- seventweets/tests/test_mock.py
+++ seventweets/tests/test_mock.py
@@ -15,6 +15,7 @@ def test_return_my_ip():
 
     mock_get_request.assert_called_once_with('https://httpbin.org/ip')
     mock_response = mock_get_request.return_value
+    mock_response.json.assert_called_once_with()
     obj = mock_response.json.return_value
     obj.__getitem__.assert_called_once_with('origin')
     assert obj.__getitem__.return_value == ip
